823dac8d954abf155f81a05d6adc4cd80c729b3a
Numa <reachbmg@gmail.com>
2024-05-22 14:22:50 -0700

    new file:   .gitignore 	new file:   Cargo.toml 	new file:   src/commands/attachment_handler.rs 	new file:   src/commands/image_effects.rs 	new file:   src/commands/mod.rs 	new file:   src/main.rs

---
+++ src/main.rs
@@ -0,0 +1,54 @@
+mod commands;
+
+use poise::serenity_prelude as serenity;
+
+type Error = Box<dyn std::error::Error + Send + Sync>;
+type Context<'a> = poise::Context<'a, Data, Error>;
+// User data, which is stored and accessible in all command invocations
+pub struct Data {}
+
+#[tokio::main]
+async fn main() {
+    env_logger::init();
+
+    let framework = poise::Framework::builder()
+        .options(poise::FrameworkOptions {
+            commands: vec![
+                commands::attachment_handler::file_details(),
+                commands::attachment_handler::totalsize(),
+            ],
+            on_error: |error| {
+                Box::pin(async move {
+                    println!("what the hell");
+                    match error {
+                        poise::FrameworkError::ArgumentParse { error, .. } => {
+                            if let Some(error) = error.downcast_ref::<serenity::RoleParseError>() {
+                                println!("Found a RoleParseError: {:?}", error);
+                            } else {
+                                println!("Not a RoleParseError :(");
+                            }
+                        }
+                        other => poise::builtins::on_error(other).await.unwrap(),
+                    }
+                })
+            },
+            ..Default::default()
+        })
+        .setup(move |ctx, _ready, framework| {
+            Box::pin(async move {
+                poise::builtins::register_globally(ctx, &framework.options().commands).await?;
+                Ok(Data {})
+            })
+        })
+        .build();
+
+    let token = std::env::var("DISCORD_TOKEN").unwrap();
+    let intents =
+        serenity::GatewayIntents::non_privileged() | serenity::GatewayIntents::MESSAGE_CONTENT;
+
+    let client = serenity::ClientBuilder::new(token, intents)
+        .framework(framework)
+        .await;
+
+    client.unwrap().start().await.unwrap()
+}
